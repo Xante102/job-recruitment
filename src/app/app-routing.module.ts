4b6ae9dbcc5e42c654fa9992d5c27a484cfc54a8
7haTSheep <93883292+7haTSheep@users.noreply.github.com>
2022-11-14 11:06:48 -0500

    about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ApplicantProfileComponent } from './pages/applicant-profile/applicant-profile.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aboutus', component: AboutUsComponent},
+  { path: 'contactus', component: ContactUsComponent},
+  { path: 'faq', component: FaqsComponent},
+  { path: 'applicant-profile', component: ApplicantProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
