dbf2b237648984273ed37cbd8c187a558c91d779
7haTSheep <93883292+7haTSheep@users.noreply.github.com>
2022-11-15 02:33:37 -0500

    contact us final

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  data = {
+    fullname: '',
+    email: '',
+    companyname: '',
+    phonenumber: '',
+    department: ''
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
