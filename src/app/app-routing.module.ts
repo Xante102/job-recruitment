e6beb3c52f6e27b7c295cc1cff1faf273c800a10
Xante102 <xantegraham.14@gmail.com>
2022-11-15 18:00:32 -0500

    added routing for login and other pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompanyProfileComponent} from './pages/company-profile/company-profile.component';
+import {HomeComponent} from './pages/home/home.component';
+import {JobFormComponent} from './pages/job-form/job-form.component';
+import {LoginComponent} from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'company-profile', component: CompanyProfileComponent},
+  { path: 'job-form', component: JobFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
